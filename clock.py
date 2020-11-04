23a989c8e1668ad5121df9b89f9437aac6f4b4c3
Tim Allen <tim@timallen.me.uk>
2020-11-04 20:43:07 +0000

    Refactoring out the time to colour code

--- clock.py
+++ clock.py
@@ -1,85 +1,48 @@
 #!/usr/bin/env python
 
-import colorsys
+# import colorsys
 import display
 import time
+import time2colour
 from datetime import datetime
 
-def get_colour(divisor, val):
-    r=255
-    g=255
-    b=255
-    x = 255/divisor
+# def get_colour(divisor, val):
+#     r=255
+#     g=255
+#     b=255
+#     x = 255/divisor
 
-    if val == divisor:
-        r = 0
-        b = 0
-        g = 255
-    elif val < divisor:
-        step =  int(x * val)
-        r = 255 - step
-        g = step
-        b = 0
-    else:
-        step = int(x * (val - divisor))
-        r = 0
-        g = 255 - step
-        b = step
+#     if val == divisor:
+#         r = 0
+#         b = 0
+#         g = 255
+#     elif val < divisor:
+#         step =  int(x * val)
+#         r = 255 - step
+#         g = step
+#         b = 0
+#     else:
+#         step = int(x * (val - divisor))
+#         r = 0
+#         g = 255 - step
+#         b = step
 
-    return [r,g,b]
+#     return [r,g,b]
 
+# def get_colour_60( val ):
+#     return get_colour(30,val)
 
-def get_colour_60( val ):
-    return get_colour(30,val)
-    # r=255
-    # g=255
-    # b=255
-    # x = 255/30
-
-    # if val == 30:
-    #     r = 0
-    #     b = 0
-    #     g = 255
-    # elif val < 30:
-    #     step =  int(x * val)
-    #     r = 255 - step
-    #     g = step
-    #     b = 0
-    # else:
-    #     step = int(x * (val - 30))
-    #     r = 0
-    #     g = 255 - step
-    #     b = step
-
-    # return [r,g,b]
-
-def get_colour_12(val):
-    return get_colour(12,val)
-    # r=255
-    # g=255
-    # b=255
-    # x = 255/12
-
-    # if val == 12:
-    #     r = 0
-    #     b = 0
-    # elif val < 12:
-    #     step = int( val * x)
-    #     r = 255 - step
-    #     g = step
-    #     b = 0
-    # else:
-    #     step = int((val - 12) * x)
-    #     r = 0
-    #     g = 255 - step
-    #     b = step
-
-    # return [r,g,b]
+# def get_colour_12(val):
+#     return get_colour(12,val)
 
 def update_display(time):
-    display.set_hour_pixels(get_colour_12(time.hour))
-    display.set_minute_pixels(get_colour_60(time.minute))
-    display.set_second_pixels(get_colour_60(time.second))
+    cols = time2colour.get_colours_for_time(time)
+    display.set_hour_pixels(cols[0])
+    display.set_minute_pixels(cols[1])
+    display.set_second_pixels(cols[2])
+    # display.set_hour_pixels(get_colour_12(time.hour))
+    # display.set_minute_pixels(get_colour_60(time.minute))
+    # display.set_second_pixels(get_colour_60(time.second))
 
 while True:
     now = datetime.now()
---
+++ time2colour.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+def get_colour(divisor, val):
+    r=255
+    g=255
+    b=255
+    x = 255/divisor
+
+    if val == divisor:
+        r = 0
+        b = 0
+        g = 255
+    elif val < divisor:
+        step =  int(x * val)
+        r = 255 - step
+        g = step
+        b = 0
+    else:
+        step = int(x * (val - divisor))
+        r = 0
+        g = 255 - step
+        b = step
+
+    return [r,g,b]
+
+def get_colour_60( val ):
+    return get_colour(30,val)
+
+def get_colour_12(val):
+    return get_colour(12,val)
+
+def get_colours_for_time(time):
+    hour = get_colour_12(time.hour)
+    minute = get_colour_60(time.minute)
+    second = get_colour_60(time.second)
+    return [hour,minute,second]
