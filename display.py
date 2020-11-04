0ea702593405af1928fa89479f84a415636816ab
Tim Allen <tim@timallen.me.uk>
2020-11-04 20:02:43 +0000

    refactoring display code - WIP

--- display.py
+++ display.py
@@ -1,36 +1,61 @@
-
-
-
 from rgbmatrix5x5 import RGBMatrix5x5
 
 matrix = RGBMatrix5x5()
 matrix.set_clear_on_exit()
 matrix.set_brightness(0.8)
 
+# TODO: Arrays for pixel co-ords
+
+hour_leds = [
+    [0,0],
+    [0,1],
+    [0,2],
+    [0,3],
+    [0,4],
+
+    [1,0],
+    [1,4],
+
+    [2,0],
+    [2,4],
+
+    [3,0],
+    [3,4],
+
+    [4,0],
+    [4,1],
+    [4,2],
+    [4,3],
+    [4,4]
+]
+
 def set_hour_pixels(col):
     r = col[0]
     g = col[1]
     b = col[2]
-    matrix.set_pixel(0,0, r,b,g)
-    matrix.set_pixel(0,1, r,b,g)
-    matrix.set_pixel(0,2, r,b,g)
-    matrix.set_pixel(0,3, r,b,g)
-    matrix.set_pixel(0,4, r,b,g)
 
-    matrix.set_pixel(1,0, r,b,g)
-    matrix.set_pixel(1,4, r,b,g)
+    for x in hour_leds:
+        matrix.set_pixel(x[0],x[1], r,b,g)
+    # matrix.set_pixel(0,0, r,b,g)
+    # matrix.set_pixel(0,1, r,b,g)
+    # matrix.set_pixel(0,2, r,b,g)
+    # matrix.set_pixel(0,3, r,b,g)
+    # matrix.set_pixel(0,4, r,b,g)
 
-    matrix.set_pixel(2,0, r,b,g)
-    matrix.set_pixel(2,4, r,b,g)
+    # matrix.set_pixel(1,0, r,b,g)
+    # matrix.set_pixel(1,4, r,b,g)
 
-    matrix.set_pixel(3,0, r,b,g)
-    matrix.set_pixel(3,4, r,b,g)
+    # matrix.set_pixel(2,0, r,b,g)
+    # matrix.set_pixel(2,4, r,b,g)
 
-    matrix.set_pixel(4,0, r,b,g)
-    matrix.set_pixel(4,1, r,b,g)
-    matrix.set_pixel(4,2, r,b,g)
-    matrix.set_pixel(4,3, r,b,g)
-    matrix.set_pixel(4,4, r,b,g)
+    # matrix.set_pixel(3,0, r,b,g)
+    # matrix.set_pixel(3,4, r,b,g)
+
+    # matrix.set_pixel(4,0, r,b,g)
+    # matrix.set_pixel(4,1, r,b,g)
+    # matrix.set_pixel(4,2, r,b,g)
+    # matrix.set_pixel(4,3, r,b,g)
+    # matrix.set_pixel(4,4, r,b,g)
 
 def set_minute_pixels(col):
     r = col[0]
